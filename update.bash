fff7fcca3d98f954e9c44898098d2b9d92aee140
Florian Weber <florian@webflo.org>
2016-06-10 21:06:22 +0200

    Split and publish tags automatically

---
+++ update.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+ssh -T git@github.com
+BASEDIR=$(dirname $0)
+
+function update_subtree_split {
+  cd $BASEDIR
+ ./subtree-split fetch
+  cd $BASEDIR/upstream
+  git checkout -f $1
+  git reset --hard origin/$1
+  git clean -fd
+  cd $BASEDIR
+  ./subtree-split push branch $1
+  ./push-tags
+  rm -rf ./upstream/.git/subtree-cache
+}
+
+update_subtree_split 8.0.x
+update_subtree_split 8.1.x
+update_subtree_split 8.2.x
