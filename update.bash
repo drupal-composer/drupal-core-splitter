b844d1700373a4897de6289011ce3520e27176fe
Florian Weber <florian.weber@ueberbit.de>
2017-03-16 21:02:29 +0100

    Update the build script

--- update.bash
+++ update.bash
@@ -2,19 +2,6 @@
 ssh -T git@github.com
 BASEDIR=$(dirname $0)
 
-function update_subtree_split {
-  cd $BASEDIR
- ./subtree-split fetch
-  cd $BASEDIR/upstream
-  git checkout -f $1
-  git reset --hard origin/$1
-  git clean -fd
-  cd $BASEDIR
-  ./subtree-split push branch $1
-  ./console push-tags-legacy $1
-  rm -rf ./upstream/.git/subtree-cache
-}
-
-update_subtree_split 8.0.x
-update_subtree_split 8.1.x
-update_subtree_split 8.2.x
+cd $BASEDIR
+./console split 8.3.x
+./console split 8.4.x
